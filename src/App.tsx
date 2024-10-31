8fb007ac02c354ced14f2e528fccc35d68df1166
Émilie T <92823139+emilie-t-ledn@users.noreply.github.com>
2024-10-31 11:17:44 -0400

    Update/oct 2024 2 (#4)
    * Upgrade packages and improve app setup
    
    * Add mantine example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import '@mantine/core/styles.css';
+
+import { Code, ColorSchemeScript, createTheme, MantineProvider } from '@mantine/core';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import styled from 'styled-components';
+
+const queryClient = new QueryClient();
+const theme = createTheme({});
+
+const Example = styled(Code)`
+  &.mantine-Code-root {
+    font-size: 2.4rem
+  }
+`
+
 const App = () => {
-  return <h1 style={{
-    width: "100%",
-    textAlign: 'center',
-  }}>Ledn frontend challenge</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ColorSchemeScript defaultColorScheme="auto" />
+      <MantineProvider theme={theme} defaultColorScheme="auto">
+        <Example>Ledn frontend challenge</Example>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </MantineProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
