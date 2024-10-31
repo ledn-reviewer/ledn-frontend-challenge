eb6292b6c90a2a7224ba9dcf7369cfc0133385f4
Emilie Touchette <emilie.touchette@ledn.io>
2024-10-31 09:55:49 -0400

    Add mantine example

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import "@mantine/core/styles.css";
+import '@mantine/core/styles.css';
 
+import { Code, ColorSchemeScript, createTheme, MantineProvider } from '@mantine/core';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { ColorSchemeScript, createTheme, MantineProvider } from '@mantine/core';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import styled from 'styled-components';
 
 const queryClient = new QueryClient();
 const theme = createTheme({});
+
+const Example = styled(Code)`
+  &.mantine-Code-root {
+    font-size: 2.4rem
+  }
+`
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ColorSchemeScript defaultColorScheme="auto" />
       <MantineProvider theme={theme} defaultColorScheme="auto">
-        Ledn frontend challenge
+        <Example>Ledn frontend challenge</Example>
         <ReactQueryDevtools initialIsOpen={false} />
       </MantineProvider>
     </QueryClientProvider>
