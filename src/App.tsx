512d382005fb62533519d80e9efe1d5f023c3e92
Tiso Alvarez Puccinelli <tiso.alvarez@ledn.io>
2026-03-20 14:40:53 -0300

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, Code, defaultSystem } from '@chakra-ui/react';
+import { ChakraProvider, Text, defaultSystem, Box } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -8,7 +8,9 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider value={defaultSystem}>
-        <Code fontSize="2xl">Ledn frontend challenge</Code>
+        <Box w="100%" h="100%" p={5} bg="gray.50">
+          <Text fontSize="2xl">Ledn frontend challenge</Text>
+        </Box>
         <ReactQueryDevtools initialIsOpen={false} />
       </ChakraProvider>
     </QueryClientProvider>
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { makeServer } from "./server";
 
+import "./styles.css";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
