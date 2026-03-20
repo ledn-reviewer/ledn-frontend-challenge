9c8c2b1f16a42fbef070e5869455b9dee1e927fe
Tiso Alvarez Puccinelli <tiso.alvarez@ledn.io>
2026-03-20 14:40:30 -0300

    Replace Mantine with Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
-import '@mantine/core/styles.css';
-
-import { Code, ColorSchemeScript, createTheme, MantineProvider } from '@mantine/core';
+import { ChakraProvider, Code, defaultSystem } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import styled from 'styled-components';
 
 const queryClient = new QueryClient();
-const theme = createTheme({});
-
-const Example = styled(Code)`
-  &.mantine-Code-root {
-    font-size: 2.4rem
-  }
-`
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ColorSchemeScript defaultColorScheme="auto" />
-      <MantineProvider theme={theme} defaultColorScheme="auto">
-        <Example>Ledn frontend challenge</Example>
+      <ChakraProvider value={defaultSystem}>
+        <Code fontSize="2xl">Ledn frontend challenge</Code>
         <ReactQueryDevtools initialIsOpen={false} />
-      </MantineProvider>
+      </ChakraProvider>
     </QueryClientProvider>
   );
 };
